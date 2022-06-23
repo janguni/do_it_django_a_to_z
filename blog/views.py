ce83e5fe95e5aa184503fd85820269c7da9cfab1
janguni <lightson23@naver.com>
2022-06-23 17:10:06 +0900

    포스트 수정 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from unicodedata import category
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Category, Post, Tag
 from django.shortcuts import render, redirect
+from django.core.exceptions import PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -37,6 +38,18 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request,*args, **kwargs)
+        else:
+            raise PermissionDenied
     
 
 
