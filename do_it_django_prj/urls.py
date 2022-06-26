481c06684bfd9ecea28bbc3953f59bec121aaca5
janguni <lightson23@naver.com>
2022-06-26 12:22:02 +0900

    Content 입력한에 마크다운 적용하기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path as url
 
 urlpatterns = [
     path('blog/',include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('markdownx/', include('markdownx.urls')),
     path('', include('single_pages.urls'))
 ]
 
