78f4c5c66efc513d2d0320bf89ae7aa15515fc4f
janguni <lightson23@naver.com>
2022-06-30 17:21:21 +0900

    검색 기능 구현하기

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from .forms import CommentForm
 from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
+from django.db.models import Q
 
 class PostList(ListView):
     model = Post
@@ -182,3 +183,20 @@ def delete_comment(request, pk):
         return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+class PostSearch(PostList):
+    paginate_by: int = None
+
+    def get_queryset(self): # 오버라이딩
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q) # 장고에서 쿼리는 . 대신 __
+        ).distinct() # 중복제거
+        return post_list
+    
+    def get_context_data(self, **kwargs): # 다른 객체들의 정보도 보내고 싶을 때
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
